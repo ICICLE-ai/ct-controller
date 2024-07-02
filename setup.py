c7cee61f668667423ba310e2305849d16d8f6e8a
Samuel Khuvis <skhuvis@osc.edu>
2024-07-02 14:16:53 -0700

    Add python-chi as dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name = 'ctcontroller',
       install_requires = [
           'paramiko==3.3.1',
           'cryptography==41.0.3',
-          'python-openstackclient==6.3.0',
+          'python-chi==0.17.11',
           'pyyaml',
       ],
 )
