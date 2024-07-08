959ab1de5e44ba738198cc40a9e96ad3830888bd
Samuel Khuvis <skhuvis@osc.edu>
2024-07-08 14:34:28 -0700

    Cleanup error module

--- ctcontroller/error.py
+++ ctcontroller/error.py
@@ -1,4 +1,16 @@
+"""
+Contains functions to gracefully handle errors.
+"""
+
+import sys
+
 def print_and_exit(msg: str):
+    """
+    Prints the error message and exits the entire application with an exit code of 1
+
+    Parameters:
+        msg (str): the message that should be printed before exiting
+    """
+
     print(f'\033[91mERROR: {msg}')
-    import sys
     sys.exit(1)
