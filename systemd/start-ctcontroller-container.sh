f10584956cbd5e900cb5693c5ca4265b5bce80d2
Samuel Khuvis <skhuvis@osc.edu>
2025-12-01 14:20:57 -0500

    add docker client timeout to systemd startup script

--- systemd/start-ctcontroller-container.sh
+++ systemd/start-ctcontroller-container.sh
@@ -4,7 +4,7 @@ IMAGE=tapis/ctcontroller:latest
 CONTAINER=ctcontroller
 PORT=8080
 
-docker pull $IMAGE
+DOCKER_CLIENT_TIMEOUT=30 docker pull $IMAGE
 CMD="docker create --privileged --name "$CONTAINER" -v /var/lib/ctcontroller:/var/lib/ctcontroller -v /var/run/docker.sock:/var/run/docker.sock -e ENV_PATH=/var/lib/ctcontroller/env -p $PORT:$PORT $IMAGE -d"
 
 # check if container already exists
